d59f0255c86d49ad91778da2090319c07eaf7b75
Daniel Felipe Rosero Zemanate <dandan32099@gmail.com>
2023-07-11 01:09:03 -0500

    BLUE LABEL

--- ProyectoADA.py
+++ ProyectoADA.py
@@ -3,19 +3,22 @@
 # Ordenar el arreglo de la apertura tomando como valor clave, el elemento de mayor valor de cada escena (el ultimo)
 # Ordenar nuevamente el arreglo de la apertura tomando como valor clave, la suma de todos los valores de cada escena
 # -----------------------------
-# Una vez ordenado eso, revisar como arreglar las partes considerando que la apertura ya está ordenada
-
+# Una vez ordenada
 # -----------------------------
 # Counting sort: O(n) porque el valor mas grande del arreglo es 3n-3
 # Recorrer el arreglo inicial y ordenar cada escena: O(3n)
 # Ordenar el arreglo por grandeza individual: O(n)
 # Ordenar el arreglo por grandeza total: O(n)
 # -----------------------------
-
 # Máximo valor que puede tomar n: indefinido
 # Máximo número de escenas posibles: (n^3-3n^2+2n)/6
 # Máximo valor que puede tomar m: depende de k en el mayor caso
 # Máximo valor que puede tomar k: depende de m en el mayor caso 
+# -----------------------------
+
+#Se crea una clase escena, a la cual pertenecerán todas las escenas que hacen parte del espectaculo.
+#Una escena cuenta con una lista de animales que participan en ella y la parte del espectaculo a la que pertenece.
+#La parte del espectaculo a la que pertenecen se asigna de forma arbitraria, este valor se utilizará luego para ordenar las escenas. #CONCLUSION, SE HACE QUE TODOS LOS INPUTS SIGAN ESA ESTRUCTURA ESPECIFICA Y SE MENCIONA EN EL INFORME, QUE TODAS LAS ENTRADAS SIGUEN ESE FORMATO 
 class Escena:
 
     def __init__(self, lista, parte):
@@ -25,16 +28,28 @@ class Escena:
     def __str__(self):
         return str(self.lista)
 
+#----------------------FUNCIONES AUXILIARES----------------------#
+
+#Funcion que suma la grandeza de los 3 animales que participan en una escena.
+#Recibe como parametros una escena y el diccionario de animales con sus respectivas grandezas.
+#Retorna la suma de las grandeza de los animales que participan en la escena.
 def sum_escena(escena,animales):
     suma = 0
     for animal in escena.lista:
         suma += animales[animal]
     return suma
 
+#Funcion que retorna el animal de mayor grandeza que participa en una escena.
+#Recibe como parametros una escena y el diccionario de animales con sus respectivas grandezas.
+#Retorna el animal de mayor grandeza que participa en la escena.
 def max_escena(escena,animales):
     maximo = animales[escena.lista[2]]
     return maximo
 
+#Funcion que ordena los 3 animales que participan en una escena de forma ascendente.
+#Recibe como parametros una escena y el diccionario de animales con sus respectivas grandezas.
+#Retorna la escena con los animales ordenados de forma ascendente.
+#
 def ordenar_escena(escena, animales):
     for i in range(1, len(escena.lista)):
         key = escena.lista[i]
@@ -47,23 +62,33 @@ def ordenar_escena(escena, animales):
     return escena
 
 animales = {
-    "Cienpies" : 1,
-    "Libelula" : 2,
-    "Gato" : 3,
-    "Perro" : 4,
-    "Tapir" : 5,
-    "Nutria" : 6,
+    "Leon" : 9,
+    "PanteraNegra" : 7,
+    "Cebra" : 6,
+    "Cocodrilo" : 5,
+    "Boa" : 4,
+    "Loro" : 2, 
+    "Caiman" : 3,
+    "Tigre" : 8,
+    "Capibara" : 1,
 }
 
 partes = [
     [
-        ["Tapir", "Nutria", "Perro"],           # Escena(["Tapir", "Nutria", "Perro"], 1)
-        ["Cienpies", "Tapir", "Gato"]           # Escena(["Cienpies", "Tapir", "Gato"], 1)
+        ["Caiman", "Capibara", "Loro"],
+        ["Tigre", "Loro", "Capibara"],
+        ["Tigre", "Cebra", "PanteraNegra"],
     ],
     [
-        ["Tapir", "Perro", "Gato"],             # Escena(["Tapir", "Perro", "Gato"], 2)
-        ["Gato", "Cienpies", "Libelula"],       # Escena(["Gato", "Cienpies", "Libelula"], 2)
+        ["PanteraNegra", "Cocodrilo", "Loro"],
+        ["Leon", "PanteraNegra", "Cebra"],
+        ["Cocodrilo", "Capibara", "Loro"]
     ],
+    [
+        ["Boa", "Caiman", "Capibara"], 
+        ["Leon", "Caiman", "Loro"],
+        ["Leon", "Cocodrilo", "Boa"]
+    ]
 ]
 
 for i, parte in enumerate(partes):
@@ -119,40 +144,47 @@ def ordenar_apertura_max_valor(entrada, animales):
 
 def ordenar_apertura(entrada, animales):
     apertura = ordenar_apertura_max_valor(entrada, animales)
+    print("----DEBUG----")
+    print([escena.lista for escena in apertura])
     sum_escenas = []
     for escena in apertura:
         sum_escenas.append(sum_escena(escena,animales))
 
+    print(sum_escenas[::-1])
     # # Encontrar el rango máximo del arreglo
     max_val = max(sum_escenas) + 1
 
     # # Inicializar un arreglo de conteo con ceros
     count = [0] * max_val
+    
+    print("count", count)
 
     # # Contar la frecuencia de cada elemento en el arreglo de entrada
     for num in sum_escenas:
         count[num] += 1
 
+    print("count2", count)
+
     # # Calcular las posiciones finales de cada elemento
     for i in range(1, max_val):
         count[i] += count[i - 1]
 
+    print("count3", count)
+
     apertura_ordenada = [0] * len(sum_escenas)
 
-    i = 0
+    i = len(sum_escenas) - 1
 
     # # Colocar cada elemento en su posición correcta en el arreglo de salida
-    for num in sum_escenas:
-        apertura_ordenada[count[num] - 1] = apertura[i]
-        i += 1
-        count[num] -= 1
+    for num in sum_escenas[::-1]:
+        apertura_ordenada[count[num] - 1] = apertura[i]   #REGALARON EL ONLY DE UNA VIEJA QUE ESTA RE BUENA, QUE BENDICION ----PASELO---- AHI VOY, ESTA EN WPP
+        i -= 1
+        count[num] -= 1 
+        print("count4", count, i+1)
 
     return apertura_ordenada
 
 ordenada = ordenar_apertura(apertura, animales)
-
-# for escena in ordenada:
-#     print(escena, escena.parte)
 
 partes_ordenadas = [[] for i in range(len(partes))]
 
@@ -197,7 +229,7 @@ def ordenar_partes(partes, animales):
     return partes_ordenadas
 
 print("Apertura")
-print([escena.lista for escena in ordenada])
+print([(escena.lista, sum_escena(escena, animales)) for escena in ordenada])
 
 print("Partes ordenadas por grandeza total")
 partes2 = ordenar_partes(partes_ordenadas, animales)
